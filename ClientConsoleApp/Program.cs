cf404fb0fa27b773da7f8c506b4fee485d3d9d66
Paweł Łęczycki <pawel@msd.info.pl>
2022-03-17 11:53:11 +0100

    HttpClient

---
+++ ClientConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            using var client = new WebApiClient("https://localhost:5001/");
+
+            var result = await client.GetStringAsync("weatherForecast");
+
+            var users = await client.GetAsync<IEnumerable<User>>("api/users");
+
+            await client.LoginAsync("api/users/login", new User { Login = "Admin", Password = "@dmin" });
+            users = await client.GetAsync<IEnumerable<User>>("api/users");
+        }
+    }
+}
---
+++ ClientConsoleApp/WebApiClient.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConsoleApp
+{
+    public class WebApiClient : IDisposable
+    {
+        private HttpClient _client;
+
+        private JsonSerializerSettings JsonSerializerSettings { get; } = new JsonSerializerSettings
+        {
+            DateFormatString = "yy.MM+dd"
+        };
+
+        public WebApiClient(string baseAddress)
+        {
+            var handler = new HttpClientHandler
+            {
+                AutomaticDecompression = System.Net.DecompressionMethods.Brotli
+            };
+
+            _client = new HttpClient(handler) { 
+                BaseAddress = new Uri(baseAddress) };
+            _client.DefaultRequestHeaders.AcceptEncoding.Add(StringWithQualityHeaderValue.Parse("br"));
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+        public async Task<T> GetAsync<T>(string request)
+        {
+            var content = await GetStringAsync(request);
+            if (content == null)
+                return default;
+            return JsonConvert.DeserializeObject<T>(content, JsonSerializerSettings);
+
+        }
+        public async Task<string> GetStringAsync(string request)
+        {
+            var response = await _client.GetAsync(request);
+            //response.EnsureSuccessStatusCode();
+            if (!response.IsSuccessStatusCode)
+                return default;
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<T> PostAsync<T>(string request, T payload)
+        {
+            var response = await _client.PostAsJsonAsync(request, payload);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<T>();
+        }
+
+        public async Task LoginAsync<T>(string request, T payload)
+        {
+            var response = await _client.PostAsJsonAsync(request, payload);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", content);
+        }
+    }
+}
