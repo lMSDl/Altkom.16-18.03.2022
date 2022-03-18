c8c1b0eb000c42139d14bd73774d82ab475eb26a
Paweł Łęczycki <pawel@msd.info.pl>
2022-03-18 13:05:16 +0100

    HelathCheck

---
+++ HealthCheck/RandomHelathCheck.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthCheck
+{
+    public class RandomHelathCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var second = DateTime.Now.Second;
+
+            if (second % 2 == 0)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy("I am OK!", new Dictionary<string, object>{{ "numberOfClients", new Random().Next() }}));
+            }
+            if (second % 3 == 0)
+                return Task.FromResult(HealthCheckResult.Degraded("HELP ME!"));
+
+            return Task.FromResult(HealthCheckResult.Unhealthy());
+        }
+    }
+}
